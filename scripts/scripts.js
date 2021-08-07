2ea94309684d8f0f2b6bcaf78e4c998e8f426d28
Nick Workman <nicholaszworkman@gmail.com>
2021-08-07 00:03:29 -0400

    August 7 updates

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    $(".orderCollapseBtn").on("click", function(){
+        let icon = $(this).children("i");
+         if (icon.hasClass("fa-angle-down")) {
+             $(icon).replaceWith('<i class="fa fa-angle-up"></i>');
+         } else {
+             $(icon).replaceWith('<i class="fa fa-angle-down"></i>');
+         }
+     });
+})
